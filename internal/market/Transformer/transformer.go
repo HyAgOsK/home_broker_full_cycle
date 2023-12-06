de66e44dc746e5080a99a5a0ce3d6e5f314dd0ea
HyAgOsK <hyagovieira@geb.inatel.br>
2023-12-05 22:06:05 -0300

    Adicionando outros detalhes

--- internal/market/Transformer/transformer.go
+++ internal/market/Transformer/transformer.go
@@ -1,8 +1,8 @@
 package transformer
 
 import (
-	"github.com/HyAgOsK/home_broker_full_cycle/tree/main/internal/market/dto"
-	"github.com/HyAgOsK/home_broker_full_cycle/tree/main/internal/market/entity"
+	"github.com/devfullcycle/imersao13/go/internal/market/dto"
+	"github.com/devfullcycle/imersao13/go/internal/market/entity"
 )
 
 func TransformInput(input dto.TradeInput) *entity.Order {
